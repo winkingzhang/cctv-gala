4ce5d336d8cabb649d84e66fc375a5c764612f84
Zhang Wenqing <winking.zhang@live.com>
2021-02-21 22:55:52 +0800

    create project from template

---
+++ src/Thoughtworks.Gala.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Thoughtworks.Gala.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/Thoughtworks.Gala.Services.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Thoughtworks.Gala.Services.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
